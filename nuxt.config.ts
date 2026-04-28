d710750fbfbb640420d5e3ec49f116133f40760e
rifqidaiva <rivkidaiva123@gmail.com>
2026-04-28 12:44:47 +0700

    Initial scaffold commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["shadcn-nuxt", "@nuxtjs/tailwindcss"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+})
